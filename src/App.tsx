a53c1d3ef9caf0c6a4cc88228e4e9474c9228672
Manoj AP <manojap.nair@gmail.com>
2022-05-28 21:57:16 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import SimpleMentionEditor from "./components/SimpleMentionEditor";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <SimpleMentionEditor />
+    </div>
+  );
+}
